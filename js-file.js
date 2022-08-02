b94a51150031194f343134a3b1bd3eac19d17729
Richard Quach <quach.rich@gmail.com>
2022-08-02 16:30:45 -0700

    Create functions to allow for user input for sizing and coloring

--- js-file.js
+++ js-file.js
@@ -1,14 +1,39 @@
-const container = document.querySelector('.container')
+function populateBoard(size) {
+    const container = document.querySelector('.container')
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement('div')
-    row.classList.add('row')
-    container.append(row)
-
-    for (let i = 0; i < 16; i++) {
-        const content = document.createElement('div')
-        content.classList.add('content')
-        row.appendChild(content)
-        container.appendChild(row)
+    while(container.firstChild) {
+        container.removeChild(container.firstChild)
     }
+
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement('div')
+        row.classList.add('row')
+        container.append(row)
+
+        for (let i = 0; i < size; i++) {
+            const square = document.createElement('div')
+            square.classList.add('square')
+            row.appendChild(square)
+            container.appendChild(row)
+        }
+    }
+}
+
+populateBoard(16)
+
+function changeSize() {
+    let userInput = window.prompt("Enter how many squares you want on each side: ")
+    if (userInput > 100 || userInput < 2) {
+        userInput = window.prompt("Enter a number less than 100 and greater than 2 again please: ")
+    }
+    populateBoard(userInput)
+}
+
+changeSize()
+
+const targetSquares = document.querySelector('.container').querySelectorAll('.square')
+for (const targetSquare of targetSquares) {
+    targetSquare.addEventListener('mouseenter', () => {
+        targetSquare.classList.add('clicked')
+    })
 }
